e5f043baf84d81fbed621d8184bc7d0bea074d54
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-06 10:45:35 +0900

    refactor: rely on brigadier for malformed restart syntax

--- common/src/main/java/net/okocraft/yaminabe/common/restart/command/RestartCommandExecutor.java
+++ common/src/main/java/net/okocraft/yaminabe/common/restart/command/RestartCommandExecutor.java
@@ -134,11 +134,6 @@ final class RestartCommandExecutor<S> {
             });
     }
 
-    int invalidArguments(S source) {
-        this.sourceAdapter.sendMessage(source, RestartCommandMessages.INVALID_ARGUMENTS);
-        return 0;
-    }
-
     private int scheduleTimed(
         S source,
         ShutdownType type,
